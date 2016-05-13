056ec34ad214752bd65881c686a629cb094c4a0b
Jaime Parra Jr <jparra@us.ibm.com>
2016-05-13 13:27:11 -0500

    Removed hardcoded url.

--- dra-check.py
+++ dra-check.py
@@ -5,12 +5,12 @@ import sys
 import requests
 import re
 import os
+from urlparse import urlparse
 
 
 
-
-if len(sys.argv) < 5:
-    print "ERROR: TOOLCHAIN_ID, BEARER, or PROJECT_NAME are not defined."
+if len(sys.argv) < 6:
+    print "ERROR: TOOLCHAIN_ID, BEARER, PROJECT_NAME, or IDS_URL are not defined."
     exit(1)
     
     
@@ -18,15 +18,21 @@ TOOLCHAIN_ID = sys.argv[1]
 BEARER = sys.argv[2]
 PROJECT_NAME = sys.argv[3]
 OUTPUT_FILE = sys.argv[4]
+IDS_URL = sys.argv[5]
 DRA_SERVICE_NAME = 'draservicebroker'
 DRA_PRESENT = False
 ORGANIZATION_GUID = ''
+CF_CONTROLLER = ''
 DRA_SERVER = ''
+DLMS_SERVER = ''
+IDS_URL = urlparse(IDS_URL)
 
-
+#print IDS_URL.scheme + "://" + IDS_URL.netloc.replace( 'devops', 'devops-api' )
+IDS_URL = IDS_URL.scheme + "://" + IDS_URL.netloc.replace( 'devops', 'devops-api' )
+#print IDS_URL
 
 try:
-    r = requests.get( 'https://devops-api.stage1.ng.bluemix.net/v1/toolchains/' + TOOLCHAIN_ID + '?include=metadata', headers={ 'Authorization': BEARER })
+    r = requests.get( IDS_URL + '/v1/toolchains/' + TOOLCHAIN_ID + '?include=metadata', headers={ 'Authorization': BEARER })
     
     data = r.json()
     #print data
@@ -43,12 +49,15 @@ try:
                     #print services[ 'service_id' ]
                     if services[ 'service_id' ] == DRA_SERVICE_NAME:
                         DRA_PRESENT = True
+                        CF_CONTROLLER = services[ 'parameters' ][ 'cf_controller' ]
+                        DRA_SERVER = services[ 'parameters' ][ 'dra_server' ]
+                        DLMS_SERVER = services[ 'parameters' ][ 'dlms_server' ]
                         #Test case
                         #services[ 'dashboard_url' ]='https://da.oneibmcloud.com/dalskdjl/ljalkdj/'
                         #print services[ 'dashboard_url' ]
-                        urlRegex = re.compile(r'http\w*://\S+?/');
-                        mo = urlRegex.search(services[ 'dashboard_url' ])
-                        DRA_SERVER = mo.group()[:-1]
+                        #urlRegex = re.compile(r'http\w*://\S+?/');
+                        #mo = urlRegex.search(services[ 'dashboard_url' ])
+                        #DRA_SERVER = mo.group()[:-1]
     else:
         #ERROR response from toolchain API
         print 'ERROR:', r.status_code, '-', data
@@ -65,7 +74,11 @@ if DRA_PRESENT:
     f = open(OUTPUT_FILE,'w')
     f.write(ORGANIZATION_GUID)
     f.write('\n')
+    f.write(CF_CONTROLLER)
+    f.write('\n')
     f.write(DRA_SERVER)
+    f.write('\n')
+    f.write(DLMS_SERVER)
     f.close()
     exit(0)
 else:
